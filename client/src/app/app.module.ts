ad54824eb5ae3312387089ea78d1c04b92e970f1
James McCants <jamesdmccants@gmail.com>
2017-02-18 13:28:41 -0600

    google sign-in works for web

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,18 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { routes } from './app.routes';
+
+import { AuthGuard } from './auth.service';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare const gapi: any;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+  googleLoginButtonId = 'google-login-button';
+
+  constructor( private _zone: NgZone,private router:Router) {}
+
+  ngAfterViewInit() { // Converts the Google login button stub to an actual button.
+    gapi.signin2.render(this.googleLoginButtonId, {
+      'onsuccess': this.onLoginSuccess,
+      'onfailure': this.onLoginFailure,
+      'scope': 'profile email',
+      'theme': 'dark',
+      'width': 240,
+      'height': 50,
+      'longtitle': true
+    });
+
+  }
+
+  private onLoginSuccess = (user: any) => {
+    this._zone.run(() => {
+      var email = user.getBasicProfile().getEmail();
+      var auth_token = user.getAuthResponse().id_token;
+      console.log(email);
+      this.router.navigate(['/members']);
+        // Use a service to store authenticate with backend
+        // Store token in local storage for authentication
+    });
+  }
+
+  private onLoginFailure = (error: any) => {
+    console.log(error); }
+  }
